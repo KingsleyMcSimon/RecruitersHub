327fc18c7be3e584347c8fbbaacdcb5a8ae70987
Kingsley McSimon <kingsley.mcSimon@gmail.com>
2021-02-22 23:07:16 +0100

    Posgresql database added and Gemfile and database.yml updated

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_04_170350) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "recruits", force: :cascade do |t|
     t.text "recruit"
     t.datetime "created_at", precision: 6, null: false
@@ -27,8 +30,11 @@ ActiveRecord::Schema.define(version: 2021_02_04_170350) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
